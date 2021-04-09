f356ece2f771ab4b772b1cec0885fa66bed0978b
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-04-09 20:20:26 -0300

    chaged data format sent to pubsub

--- functions/publish.js
+++ functions/publish.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 const { PubSub } = require('@google-cloud/pubsub');
 const pubsub = new PubSub();
 
 async function publishMessage(orderData) {
     const date = new Date();
-    // const data = `O pedido ${order_id} foi pago no dia ${date}`;
     const data = {"paymentDate": date, "orderInfo": orderData};
-    const dataBuffer = Buffer.from(JSON.stringify(data));
-    const topicName = 'projects/dev-overlaycat/topics/paid-orders';
+    // const dataBuffer = Buffer.from(JSON.stringify(data));
+    // const topicName = 'paid-orders';
 
     try {
-        const messageId = await pubsub.topic(topicName).publish(dataBuffer);
+        //const messageId = await pubsub.topic(topicName).publish(dataBuffer);
+        const messageId = await pubsub.topic('paid-orders').publishJSON(data);
         console.log(`Mensagem ${messageId} publicada`);
     } catch (error) {
         console.error(`Erro recebido ao publicar: ${error.message}`);
-        process.exitCode = 1;
+        throw error
     }
 }
 
