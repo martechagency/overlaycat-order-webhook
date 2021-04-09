9656a8769885b5d769e3a9b098d463c0797e0317
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-04-09 20:21:20 -0300

    fixed json object indexes

--- functions/build-email.js
+++ functions/build-email.js
@@ -1,11 +1,11 @@
 function mailBuilder(data) {
-    const toAddress = `${data['customer']['name']} <${data['customer']['email']}>`;
-    const subject = `PEDIDO ${data['id']} - Seu Stream Package chegou!`;
+    const toAddress = `${data['orderInfo']['customer']['name']} <${data['orderInfo']['customer']['email']}>`;
+    const subject = `PEDIDO ${data['orderInfo']['id']} - Seu Stream Package chegou!`;
     let msg = '';
-    msg += `<h1>Recebemos o pagamento de seu pedido.</h1><p>Baixe os arquivos de seu pedido ${data['id']} nos links a seguir: </p>`;
+    msg += `<h1>Recebemos o pagamento de seu pedido.</h1><p>Baixe os arquivos de seu pedido ${data['orderInfo']['id']} nos links a seguir: </p>`;
     msg += '<p>';
-    for (let i = 0; i < data['products'].length; i++) {
-        const produto = data['products'][i]['name'];
+    for (let i = 0; i < data['orderInfo']['products'].length; i++) {
+        const produto = data['orderInfo']['products'][i]['name'];
         msg += `<a>${produto}</a><br>`
     }
     msg += '</p>'
