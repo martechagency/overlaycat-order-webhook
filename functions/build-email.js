bb98f450d2ca18c570931ce92ece629cf04760da
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-04-23 10:33:52 -0300

    Added the zip file link for download the pack imgs

--- functions/build-email.js
+++ functions/build-email.js
@@ -1,13 +1,14 @@
-function mailBuilder(data) {
+function mailBuilder(data, link) {
     const toAddress = `${data['orderInfo']['customer']['name']} <${data['orderInfo']['customer']['email']}>`;
     const subject = `PEDIDO ${data['orderInfo']['id']} - Seu Stream Package chegou!`;
     let msg = '';
-    msg += `<h1>Recebemos o pagamento de seu pedido.</h1><p>Baixe os arquivos de seu pedido ${data['orderInfo']['id']} nos links a seguir: </p>`;
+    msg += `<h1>Recebemos o pagamento de seu pedido.</h1><p>Baixe os arquivos de seu pedido pelo link: </p>`;
     msg += '<p>';
-    for (let i = 0; i < data['orderInfo']['products'].length; i++) {
-        const produto = data['orderInfo']['products'][i]['name'];
-        msg += `<a>${produto}</a><br>`
-    }
+    // for (let i = 0; i < data['orderInfo']['products'].length; i++) {
+    //     const produto = data['orderInfo']['products'][i]['name'];
+    //     msg += `<a>${produto}</a><br>`
+    // }
+    msg += link
     msg += '</p>'
     return [toAddress, subject, msg];
 }
