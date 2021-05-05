ad01975e475f59497ea49f74dd298bd100f64ddb
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-05-05 15:32:03 -0300

    removed unnecessary console log

--- functions/publish.js
+++ functions/publish.js
@@ -5,14 +5,14 @@ const pubsub = new PubSub();
 
 async function publishMessage(orderData) {
     const date = new Date();
-    const data = {"paymentDate": date, "orderInfo": orderData};
+    const data = { "paymentDate": date, "orderInfo": orderData };
     // const dataBuffer = Buffer.from(JSON.stringify(data));
     // const topicName = 'paid-orders';
 
     try {
         //const messageId = await pubsub.topic(topicName).publish(dataBuffer);
         const messageId = await pubsub.topic('paid-orders').publishJSON(data);
-        console.log(`Mensagem ${messageId} publicada`);
+        // console.log(`Mensagem ${messageId} publicada`);
     } catch (error) {
         console.error(`Erro recebido ao publicar: ${error.message}`);
         throw error
